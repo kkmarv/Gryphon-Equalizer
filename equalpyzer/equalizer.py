1c142e75042662bbb3e9db79279f89b89b02dad6
Marvin <kaesting.marvin@student.hs-anhalt.de>
2021-12-13 20:40:39 +0100

    rewrite equalizer.py - new class Equalizer

--- equalpyzer/equalizer.py
+++ equalpyzer/equalizer.py
@@ -2,16 +2,81 @@ import numpy as np
 
 from numpy import ndarray
 from scipy.io import wavfile as wf
-
-from typing import Tuple
 from matplotlib import pyplot as plt
+
+from typing import Tuple, Union
+
+
+class Equalizer:
+    def __init__(self):
+        self.__sample_rate: Union[int, None] = None
+        self.__time_domain: Union[ndarray, None] = None
+        self.__freq_domain: Union[ndarray, None] = None
+        self.__normalizer: Union[int, None] = None  # loudest sample in the file
+
+    def load_signal(self, path: str) -> None:
+        self.__sample_rate, self.__time_domain = wf.read(path)
+        self.__normalizer = np.max(self.__time_domain)
+
+    def plot_time_domain(self) -> None:
+        n: int = len(self.__time_domain)  # number of samples
+        sec: float = n / self.__sample_rate  # length of file in seconds
+
+        x_values: ndarray = self.__time_domain / self.__normalizer  # normalize values
+        y_values: ndarray = np.linspace(start=0, stop=sec, num=n)  # calculate the scaling of y axis
+
+        # plot graph and set visual parameters
+        plt.plot(y_values, x_values)
+        plt.xlim(left=0, right=sec)
+        plt.ylim(top=1, bottom=-1)
+        plt.xlabel('Time [sec]')
+        plt.ylabel('Amplitude')
+        plt.grid(True)
+        plt.show()
+
+    def plot_freq_domain(self) -> None:
+        n = len(self.__time_domain)  # number of time domain samples
+        sec: float = n / self.__sample_rate  # length of file in seconds
+
+        # scale the magnitude of the fft by and factor of 2,
+        # because we are using only half of the fft spectrum
+        # and normalize it by the number of frequency samples
+        scaled_x_values = np.abs(self.freq_domain) * 2 / len(self.freq_domain)
+
+        # convert to dBFS (decibels full scale)
+        db_x_values = 20 * np.log10(scaled_x_values / self.__normalizer)
+
+        # calculate the scaling of y axis
+        y_values = np.arange((n / 2) + 1) / sec
+
+        # frequency labels
+        x_ticks = [31, 62, 125, 250, 500, 1000, 2000, 4000, 8000, 16000]
+        x_labels = ['31', '62', '125', '250', '500', '1k', '2k', '4k', '8k', '16k']
+
+        # plot graph and set visual parameters
+        plt.plot(y_values, db_x_values)
+        plt.grid(True)
+        plt.xscale('log')
+        plt.xticks(x_ticks, x_labels)
+        plt.xlim(right=20000)
+        plt.ylim(top=0, bottom=-90)
+        plt.xlabel('Frequency [Hz]')
+        plt.ylabel('Amplitude [dBFS]')
+        plt.fill_between(y_values, db_x_values, -90)
+        plt.show()
+
+    @property
+    def freq_domain(self) -> ndarray:
+        if self.__freq_domain is None:  # if we did not calculate the frequency domain yet
+            self.__freq_domain = np.fft.rfft(self.__time_domain)
+        return self.__freq_domain
 
 
 def dbfs_rfft(signal: ndarray, fs: int, window: ndarray = None, ref: int = 32768) -> Tuple[ndarray, ndarray]:
     """
     Calculate spectrum in dBFS (decibels full scale)
 
-    Args: 
+    Args:
         signal: input signal
         fs: sampling frequency
         window: vector containing window samples (same length as x).
@@ -19,8 +84,7 @@ def dbfs_rfft(signal: ndarray, fs: int, window: ndarray = None, ref: int = 32768
         ref: reference value used for dBFS scale. 32768 for int16, 8388608 for int24 and 1 for float
 
     Returns:
-        frequencies: frequency vector
-        dbfs_amp: spectrum in dBFS scale
+        frequencies: frequency vector & dbfs_amp: spectrum in dBFS scale
     """
 
     N = len(signal)  # Length of input sequence
@@ -44,102 +108,18 @@ def dbfs_rfft(signal: ndarray, fs: int, window: ndarray = None, ref: int = 32768
     return frequencies, dbfs_amp
 
 
-def plot_dbfs(freq: ndarray, dbfs_amp: ndarray, x_scale='log') -> None:
-    """
-    Plot spectrum in dBFS scale
-
-    Args:
-        freq: input frequencies
-        dbfs_amp: input amplitudes in dBFS scale
-        x_scale: x-axis scaling . 'log' for logarithmic and 'lin' for linear
-
-    Returns: None
-    """
-
-    # frequency labels
-    xticks = [31, 62, 125, 250, 500, 1000, 2000, 4000, 8000, 16000]
-    xlabels = ['31', '62', '125', '250', '500', '1k', '2k', '4k', '8k', '16k']
-
-    plt.plot(freq, dbfs_amp)
-    plt.grid(True)
-    plt.xscale(x_scale)
-    plt.xticks(xticks, xlabels)
-    plt.xlim(right=20000)
-    plt.ylim(top=0)
-    plt.xlabel('Frequency [Hz]')
-    plt.ylabel('Amplitude [dBFS]')
-    plt.show()
-
-
-def plot_dbfs_custom(freq: ndarray, dbfs_amp: ndarray, xscale='log') -> None:
-    """
-    Plot spectrum in dBFS scale
-
-    Args:
-        freq: input frequencies
-        dbfs_amp: input amplitudes in dBFS scale
-        xscale: x-axis scaling . Use 'log' for logarithmic and 'lin' for linear
-
-    Returns: None
-    """
-
-    # frequency labels
-    x_ticks = [31, 62, 125, 250, 500, 1000, 2000, 4000, 8000, 16000]
-    x_labels = ['31', '62', '125', '250', '500', '1k', '2k', '4k', '8k', '16k']
-    color = 'black'
-
-    plt.plot(freq, dbfs_amp, color=color)
-    plt.grid(True, color=color)
-    plt.xscale(xscale)
-    plt.xticks(x_ticks, x_labels)
-    plt.xlim(right=20000)
-    plt.ylim(top=0, bottom=-90)
-    plt.xlabel('Frequency [Hz]')
-    plt.ylabel('Amplitude [dBFS]')
-    plt.fill_between(freq, dbfs_amp, -90, color=color)
-    plt.show()
-
-
-def equalizer_10_band():
-    pass
-
-
-def get_channels(data: ndarray) -> int:
-    return len(data.shape)  # stereo is 2d while mono is 1d
-
-
-def get_bit_depth(data: ndarray) -> np.dtype:
-    return data.dtype  # return ndarray's data type
-
-
-def get_length_in_sec(sample_rate: int, data: ndarray) -> float:
-    return data.shape[0] / sample_rate
-
-
-def normalize(data: ndarray) -> ndarray:
-    return data / (np.max(np.abs(data), axis=0))
-
-
 def main():
-    sample_rate_16_s, data_16_s = wf.read('../examples/16-bit-signed-stereo.wav')
-    sample_rate_16_m, data_16_m = wf.read('../examples/16-bit-signed-mono.wav')
-    sample_rate_24_s, data_24_s = wf.read('../examples/24-bit-signed-stereo.wav')
-    sample_rate_24_m, data_24_m = wf.read('../examples/24-bit-signed-mono.wav')
-    sample_rate_32_s, data_32_s = wf.read('../examples/32-bit-float-stereo.wav')
-    sample_rate_32_m, data_32_m = wf.read('../examples/32-bit-float-mono.wav')
+    a = '../examples/16-bit-signed-stereo.wav'
+    b = '../examples/16-bit-signed-mono.wav'
+    c = '../examples/24-bit-signed-stereo.wav'
+    d = '../examples/24-bit-signed-mono.wav'
+    e = '../examples/32-bit-float-stereo.wav'
+    f = '../examples/32-bit-float-mono.wav'
 
-    # Take slice
-    N = 8192
-    win = np.hamming(N)
-
-    freq, dbfs_amp = dbfs_rfft(data_16_m[0:N], sample_rate_16_m, win, ref=32768)
-    plot_dbfs_custom(freq, dbfs_amp)
-
-    x_values = np.linspace(0, 10, num=10 * 48000)
-    y_values = np.sin(2 * np.pi * 1000 * x_values)
-
-    freq, dbfs_amp = dbfs_rfft(y_values, 48000)
-    plot_dbfs_custom(freq, dbfs_amp)
+    eq = Equalizer()
+    eq.load_signal(b)
+    eq.plot_time_domain()
+    eq.plot_freq_domain()
 
 
 if __name__ == "__main__":
--- equalpyzer/filter.py
+++ equalpyzer/filter.py
@@ -11,7 +11,7 @@ from matplotlib import pyplot as plt
 
 
 # cut-offs for 10 bandpass filters
-super_lows = (0, 31)
+super_lows = (1, 31)
 lows = (32, 62)
 higher_lows = (63, 125)
 lower_mids = (126, 250)
@@ -48,16 +48,36 @@ def bandpass_filter(signal: ndarray, cutoffs: Tuple[int, int], fs: int, order: i
     return sosfilt(sos_filter, signal)
 
 
-def main():
-    # Take slice
-    N = 8192
-    win = np.hamming(N)
+def bandpass_10band_filter(signal: ndarray, fs: int,
+                           gain1: float = 1, gain2: float = 1, gain3: float = 1, gain4: float = 1, gain5: float = 1,
+                           gain6: float = 1, gain7: float = 1, gain8: float = 1, gain9: float = 1, gain10: float = 1):
+    band1 = bandpass_filter(signal, super_lows, fs) * gain1
+    band2 = bandpass_filter(signal, lows, fs) * gain2
+    band3 = bandpass_filter(signal, higher_lows, fs) * gain3
+    band4 = bandpass_filter(signal, lower_mids, fs) * gain4
+    band5 = bandpass_filter(signal, mids, fs) * gain5
+    band6 = bandpass_filter(signal, higher_mids, fs) * gain6
+    band7 = bandpass_filter(signal, lower_highs, fs) * gain7
+    band8 = bandpass_filter(signal, highs, fs) * gain8
+    band9 = bandpass_filter(signal, super_highs, fs) * gain9
+    # band10 = bandpass_filter(signal, ultra_highs, fs) * gain10
 
+    return band1 + band2 + band3 + band4 + band5 + band6 + band7 + band8 + band9  # + band10
+
+
+def main():
     fs, signal = wf.read('../examples/16-bit-signed-mono.wav')
-    filtered_signal = bandpass_filter(signal, highs, fs)
+
+    freq_vector, amp_vector = dbfs_rfft(signal, fs)
+    plot_dbfs(freq_vector, amp_vector)
+
+    # filtered_signal = bandpass_filter(signal, (1, 1000), fs)
+    filtered_signal = bandpass_10band_filter(signal, fs)
+
     frequencies, dbfs_amp = dbfs_rfft(filtered_signal[0:N], fs, win)
     plot_dbfs(frequencies, dbfs_amp)
-    wf.write('./examples/rawr.wav', fs, filtered_signal)
+
+    wf.write('irfft.wav', fs, np.fft.irfft(signal))
 
 
 if __name__ == "__main__":
