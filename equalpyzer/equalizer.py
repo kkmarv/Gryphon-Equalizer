5b9b135a01f466dd10b2ce16466ce1588916b008
Marvin <kaesting.marvin@student.hs-anhalt.de>
2021-12-15 21:31:54 +0100

    improved save_signal and minor code improvements

--- equalpyzer/equalizer.py
+++ equalpyzer/equalizer.py
@@ -9,7 +9,7 @@ from typing import Union
 
 class Equalizer:
     def __init__(self):
-        self._signal: Union[ndarray, None] = None
+        self._input_signal: Union[ndarray, None] = None
         self._sample_rate: Union[int, None] = None
         self._normalizer: Union[int, None] = None  # value of loudest sample in the signal
 
@@ -17,16 +17,25 @@ class Equalizer:
         self._frequencies: Union[ndarray, None] = None
         self._amplitudes_db: Union[ndarray, None] = None
 
-        # frequency labels
+        # frequency labels for logarithmic x axis
         self._x_ticks = [20, 31, 62, 125, 250, 500, 1000, 2000, 4000, 8000, 16000]
         self._x_labels = ['20', '31', '62', '125', '250', '500', '1k', '2k', '4k', '8k', '16k']
 
     def load_signal(self, path: str) -> None:
-        self._sample_rate, self._signal = wf.read(path)
-        self._normalizer = np.max(self._signal)
+        self._sample_rate, self._input_signal = wf.read(path)
+        self._normalizer = np.max(self._input_signal)
 
     def save_signal(self, path: str) -> None:
-        wf.write(path, self._sample_rate, np.round(np.fft.irfft(self.amplitudes)).astype('int16'))
+        if self._amplitudes is None:  # if we did not fft the signal, just save it
+            # this causes 24bit files to convert to 32bit files because numpy is missing 24bit data types
+            wf.write(path, self._sample_rate, self._input_signal.astype(self._input_signal.dtype))
+        else:
+            output_signal: ndarray = np.fft.irfft(self.amplitudes)
+
+            if isinstance(self._input_signal.dtype, np.integer):
+                output_signal = np.round(output_signal)
+
+            wf.write(path, self._sample_rate, output_signal.astype(self._input_signal.dtype))
 
     def amplify(self, gain: float, low_cut: int = 0, high_cut: Union[int, None] = None):
         if high_cut is None:
@@ -38,10 +47,10 @@ class Equalizer:
         self._amplitudes_db = None  # delete the old db scale
 
     def plot_time_domain(self) -> None:
-        n: int = len(self._signal)  # number of samples
+        n: int = len(self._input_signal)  # number of samples
         sec: float = n / self._sample_rate  # length of signal in seconds
 
-        y_values: ndarray = self._signal / self._normalizer  # normalize values to [-1, 1]
+        y_values: ndarray = self._input_signal / self._normalizer  # normalize values to [-1, 1]
         x_values: ndarray = np.linspace(start=0, stop=sec, num=n)  # calculate the sampling on x axis
 
         # plot graph and set visual parameters
@@ -87,14 +96,14 @@ class Equalizer:
     def amplitudes(self) -> ndarray:
         """Amplitudes from FFT"""
         if self._amplitudes is None:  # if we did not calculate the frequency domain yet
-            self._amplitudes = np.fft.rfft(self._signal)
+            self._amplitudes = np.fft.rfft(self._input_signal)
         return self._amplitudes
 
     @property
     def frequencies(self) -> ndarray:
         """Frequencies from FFT"""
         if self._frequencies is None:
-            self._frequencies = np.fft.rfftfreq(self._signal.size, 1 / self._sample_rate)
+            self._frequencies = np.fft.rfftfreq(self._input_signal.size, 1 / self._sample_rate)
         return self._frequencies
 
     @property
@@ -119,14 +128,18 @@ def main():
     d = '../examples/24-bit-signed-mono.wav'
     e = '../examples/32-bit-float-stereo.wav'
     f = '../examples/32-bit-float-mono.wav'
+    linda = '../examples/drumloop.wav'
 
     eq = Equalizer()
-    eq.load_signal(b)
+    eq.load_signal(f)
+
+    eq.plot_time_domain()
 
     eq.plot_freq_domain()
     eq.plot_freq_domain_db()
 
-    eq.amplify(0, 0, 200)
+    # eq.amplify(0, high_cut=100)
+    # eq.amplify(0, low_cut=101)
 
     eq.plot_freq_domain()
     eq.plot_freq_domain_db()
